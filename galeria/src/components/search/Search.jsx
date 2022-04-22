4a6251a832f5b84c9de5220eaa3aadf34b4820cf
Eduardo Caparros <educaparrosbarcelo@gmail.com>
2022-04-22 09:21:12 +0200

    Search and home

--- galeria/src/components/search/Search.jsx
+++ galeria/src/components/search/Search.jsx
@@ -1,14 +1,29 @@
+import { useLocation } from 'wouter'
 import './search.css'
 
 const Search = () => {
+	const [location, setLocation] = useLocation()
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const form = e.target
+		const data = Object.fromEntries(new FormData(form))
+		let search = data.search.replace(' ', '-')
+
+		form.reset()
+
+		setLocation(`/${search}`)
+	}
+
 	return (
-		<div className='search'>
+		<form className='search' onSubmit={handleSubmit}>
 			<input
 				className='search_input'
 				type='text'
 				placeholder='Busca alguna categoria'
+				name='search'
 			/>
-		</div>
+		</form>
 	)
 }
 
--- galeria/src/pages/Home.jsx
+++ galeria/src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import Header from '../containers/header/Header'
+import Nav from '../containers/header/Nav'
+
+import '../styles/home.css'
+
 const Home = () => {
-	return <div>hola</div>
+	return (
+		<>
+			<Header />
+
+			<main>
+				<h1 className='home_title'>Bienvenido a Gifgram</h1>
+				<h2 className='home_subtitle'>Tu galería de gifs de confianza</h2>
+			</main>
+		</>
+	)
 }
 
 export default Home
